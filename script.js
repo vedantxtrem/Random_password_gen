0d5a84e502711b179588273c4a74b6fb20b36e82
Vedant Sahu <vedant@pop-os.localdomain>
2023-12-04 21:04:01 +0530

    v

--- script.js
+++ script.js
@@ -1,65 +1,46 @@
- const n = document.querySelector("#length");
+// const n = document.querySelector("#length");
 
-// function bigchecked() {
-//   let upperCase;
-//   if (document.querySelector("#uper").checked ) {
-//     return upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   }
-//   return "";
-// }
+const upperCase = "";
+const lowerCase = "";
+const number = "";
+const symbol = "";
 
-// function smchecked() {
-//   let lowerCase;
-//   if (document.querySelector("#lower").checked) {
-//     return lowerCase = "abcdefghijklmnopqrstuvwxyz";
-//   }
-//   return "";
-// }
 
-// function numchecked() {
-//   let number;
-//   if (document.querySelector("#num").checked ) {
-//     return number = "0123456789";
-//   }
-//   return "";
-// }
-// function sigchecked() {
-//   let symbol;
-//   if (document.querySelector("#symbol").checked ) {
-//     return symbol = "@#$%^&*()_-+={}[].,<>";
-//   }
-//   return "";
-// }
- 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "@#$%^&*()_-+={}[].,<>";
-
+console.log(document.querySelector("#uper").checked);
 
 const size = 6;
 
-// const upperCase = bigchecked();
-// const lowerCase = smchecked();
-// const number = numchecked();
-// const symbol = sigchecked();
 
-const allChars =  upperCase + lowerCase + number + symbol;
+
+const uper = document.querySelector("#uper");
+uper.addEventListener("checked", upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+const lower = document.querySelector("#lower");
+lower.addEventListener("checked", lowerCase = "abcdefghijklmnopqrstuvwxyz");
+
+const num = document.querySelector("#num");
+num.addEventListener("checked", number = "0123456789");
+
+const sym = document.querySelector("#symbol");
+sym.addEventListener("checked", symbol = "@#$%^&*()_-+={}[].,<>");
+
+
+const allChars = upperCase + lowerCase + number + symbol;
+console.log(allChars);
 
 function createPassword() {
-    let Password = "";
-    Password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    Password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    Password += number[Math.floor(Math.random() * number.length)];
-    Password += symbol[Math.floor(Math.random() * symbol.length)];
-    
-    while (size > Password.length) {
-        Password += allChars[Math.floor(Math.random() * allChars.length)];
-    }
+  let Password = "";
+  Password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  Password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  Password += number[Math.floor(Math.random() * number.length)];
+  Password += symbol[Math.floor(Math.random() * symbol.length)];
 
-    return Password;
+  while (size > Password.length) {
+    Password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  return Password;
 }
-
 
 const btn = document.getElementById("button");
 
